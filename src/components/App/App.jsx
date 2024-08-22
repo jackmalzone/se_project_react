9f8be375c858574a79962df6c3d9519c0606616c
JAK <56848280+jackmalzone@users.noreply.github.com>
2024-08-21 20:44:30 -0700

    basic markup modal, clothes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,73 @@
+import { useState } from "react";
+import "../../utils/index.css";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+  const [activeModal, setActiveModal] = useState("");
+
+  const handleAddButtonClick = () => {
+    setActiveModal("opened");
+  };
+
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
+
   return (
     <div className="page">
       <div className="page__content">
-        <Header />
-        <Main />
+        <Header onAddButtonClick={setActiveModal} />
+        <Main weatherData={weatherData} />
         <Footer />
       </div>
+      <ModalWithForm
+        buttonText="Add Garment"
+        title="New Garment"
+        activeModal={activeModal}
+        closeActiveModal={closeActiveModal}
+      >
+        <label htmlFor="name" className="modal__label modal__label_type_input">
+          Name{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css";
 import headerLogo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar-placeholder.png";
 
-function Header() {
+function Header({ handleAddButtonClick }) {
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
@@ -12,7 +12,13 @@ function Header() {
     <header className="header">
       <img src={headerLogo} alt="" className="header__logo" />
       <p className="header__spacetime">{`${currentDate}, Location`}</p>
-      <button className="header__button-add-clothes">+ Add Clothes</button>
+      <button
+        onClick={handleAddButtonClick}
+        type="button"
+        className="header__button-add-clothes"
+      >
+        + Add Clothes
+      </button>
       <div className="header__profile">
         <p className="header__username">Name</p>
         <img src={avatar} alt="" className="header__avatar" />
