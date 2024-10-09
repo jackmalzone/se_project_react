ba12456b8d1e54a7817544efc5b05559f4c8fa4f
JAK <56848280+jackmalzone@users.noreply.github.com>
2024-10-08 21:47:40 -0700

    Refactor API utils: Add checkResponse and request functions for DRY code

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -67,3 +67,52 @@ const ModalWithForm = React.forwardRef(
 );
 
 export default ModalWithForm;
+
+// COULD BE IMPROVED
+
+// import { useEffect } from "react";
+
+// export const Modal = ({ name, onClose, children }) => {
+//   // here is `useEffect` for the `Escape` listener
+//   useEffect(() => {
+//     // we should define the handler inside `useEffect`, so that it wouldn’t lose the reference to be able to remove it
+//     const handleEscape = (e) => {
+//       if (e.key === "Escape") {
+//         onClose();
+//       }
+//     };
+
+//     document.addEventListener("keydown", handleEscape);
+//     // don’t forget to remove the listener in the `clean-up` function
+//     return () => document.removeEventListener("keydown", handleEscape);
+//   }, [onClose]);
+
+//   // here is the overlay handler
+//   const handleOverlay = (e) => {
+//     if (e.target === e.currentTarget) {
+//       onClose();
+//     }
+//   };
+
+//   // then we add the main wrapper with class `modal`
+//   return (
+//     <div className={`modal modal_type_${name}`} onClick={handleOverlay}>
+//       {/* the container for the contents */}
+//       <div className="modal__container">
+//         {/* here will be anything you add as `children`*/}
+//         {children}
+//         {/* add the close button */}
+//         <button className="modal__close" type="button" onClick={onClose} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// And now you can use Modal for any popup in the project:  ItemModal  and ModalWithForm
+
+// function ModalWithForm({ name, onClose, ...props}) {
+//   return (
+//     <Modal name={name} onClose={onClose}>
+//         <h2 className='popup__title'>{props.title}</h2>
+
+// You can make also Input for inputs and Form for any form in the project.
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,39 +1,38 @@
 const baseUrl = "http://localhost:3001";
 
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error: ${res.status}`);
+}
+
+function request(url, options) {
+  return fetch(url, options).then(checkResponse);
+}
+
 function getItems() {
-  return fetch(`${baseUrl}/items`).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-  });
+  return request(`${baseUrl}/items`);
 }
 
 export { getItems };
 
 export const addItem = (item) => {
   console.log("Sending item to server:", JSON.stringify(item, null, 2));
-  return fetch(`${baseUrl}/items`, {
+  return request(`${baseUrl}/items`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(item),
-  }).then((res) => {
-    if (res.ok) {
-      return res.json().then((data) => {
-        console.log("Server response:", JSON.stringify(data, null, 2));
-        return data;
-      });
-    }
-    return Promise.reject(`Error: ${res.status}`);
+  }).then((data) => {
+    console.log("Server response:", JSON.stringify(data, null, 2));
+    return data;
   });
 };
 
 export const deleteItem = (id) => {
-  return fetch(`${baseUrl}/items/${id}`, {
+  return request(`${baseUrl}/items/${id}`, {
     method: "DELETE",
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Error: ${res.status}`);
   });
 };
