fde1f9d80c6f51337d7bda03d78d31c8786ba1eb
JAK <56848280+jackmalzone@users.noreply.github.com>
2024-12-02 18:31:16 -0800

    submit

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,27 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "./ModalWithForm.css";
 import { useEscape } from "../../hooks/useEscape";
 import { useOverlayClick } from "../../hooks/useOverlayClick";
 
 const ModalWithForm = React.forwardRef(
-  (
-    {
-      title,
-      children,
-      buttonText,
-      onClose,
-      onSubmit,
-      isValid,
-      isLoading,
-      extraButton,
-    },
-    ref
-  ) => {
+  ({
+    title,
+    children,
+    buttonText,
+    onClose,
+    onSubmit,
+    isValid,
+    isLoading,
+    extraButton,
+  }) => {
+    const modalRef = useRef(null);
+
     useEscape(onClose);
-    useOverlayClick(ref, onClose);
+    useOverlayClick(modalRef, onClose);
 
     return (
-      <div className="modal modal_type_form modal_opened" ref={ref}>
+      <div className="modal modal_type_form modal_opened" ref={modalRef}>
         <div className="modal__content">
           <button
             type="button"
