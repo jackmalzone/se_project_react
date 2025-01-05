c818f04fd7a12a141b7e73d4c92cfb0f606f1a47
JAK <56848280+jackmalzone@users.noreply.github.com>
2025-01-05 12:41:13 -0800

    correct configcontext link

--- src/contexts/ConfigContext.js
+++ src/contexts/ConfigContext.js
@@ -3,6 +3,6 @@ import { createContext } from "react";
 export const ConfigContext = createContext({
   baseUrl:
     process.env.NODE_ENV === "production"
-      ? "https://api.your-domain.com"
+      ? "https://api.wtwrwtwr.twilightparadox.com"
       : "http://localhost:3001",
 });
